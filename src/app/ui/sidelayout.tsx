5339ddd6b177928a73049d1aef13f14065d14f55
NTKM10 <muaysiam48@gmail.com>
2024-07-10 10:52:44 +0700

    add

--- src/app/ui/sidelayout.tsx
+++ src/app/ui/sidelayout.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
-import { usePathname } from 'next/navigation';
-import Image from 'next/image';
-import { Layout, Menu, Flex } from 'antd';
-import type { MenuProps } from 'antd';
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Layout, Menu, Flex } from "antd";
+import type { MenuProps } from "antd";
 import {
   ApartmentOutlined,
   DesktopOutlined,
@@ -11,18 +14,20 @@ import {
   SettingFilled,
   UserAddOutlined,
   BookOutlined
-} from '@ant-design/icons';
-import t from "@/app/locales/jp/common.json";//import language
+} from "@ant-design/icons";
+import t from "@/app/locales/jp/common.json"; //import language
+
+
 
 const { Sider } = Layout;
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
   label: React.ReactNode,
   key: React.Key,
   icon?: React.ReactNode,
-  children?: MenuItem[],
+  children?: MenuItem[]
 ): MenuItem {
   return {
     label,
@@ -33,11 +38,11 @@ function getItem(
 }
 
 const boxStyle: React.CSSProperties = {
-  width: '90%',
-  margin: '10px auto',
+  width: "90%",
+  margin: "10px auto",
   borderRadius: 6,
-  border: '0px',
-  backgroundColor:'#FFF',
+  border: "0px",
+  backgroundColor: "#FFF",
 };
 
 const items: MenuItem[] = [
@@ -58,44 +63,37 @@ const items: MenuItem[] = [
   ]),
 ];
 
+
 const SideLayout: React.FC = () => {
-  const path = usePathname();
-  const paths = path.split('/');
-  const selectedKey = paths[paths.length - 1];
-  let openKey = "setting";
-  
-  if(paths.length > 2){
-    openKey = paths[paths.length - 2]
+  const [collapsed, setCollapsed] = useState(false);
+  const router = useRouter();
+
+  const menuClick = (key: string) => {
+    console.log("key : " + key);
+    router.push("/" + key); // Use Next.js router to navigate to the specified route
   }
-    const [collapsed, setCollapsed] = useState(false);
 
-    function menuClick(key: string[]) {
-      const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-    
-      let path = "";
-      for (let val in key) {
-        if (path != "") {
-          path = "/" + path;
-        }
-        path = key[val] + path;
-      }
-      window.location.assign(`${BASE_URL}/${path}`);
-      return false;
-    }
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+    >
+      <div className="demo-logo-vertical" />
+      <Flex style={boxStyle} justify={"center"} align={"center"}>
+        <Image src="/asset/images/logo.png" width={60} height={33} alt="Logo" />
+      </Flex>
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={["1"]}
+        mode="inline"
+        items={items}
+        onClick={(e) => {
+          menuClick(e.key);
+        }}
+      />
+    </Sider>
+  );
+};
 
-    return (
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-          <div className="demo-logo-vertical" />
-        <Flex style={boxStyle} justify={'center'} align={'center'}>
-          <Image
-            src="/asset/images/logo.png"
-            width={60}
-            height={33}
-            alt="Logo"
-          />
-        </Flex>
-        <Menu theme="dark" defaultOpenKeys={[openKey]} selectedKeys={[selectedKey]} mode="inline" items={items} onClick={(e) => {menuClick(e.keyPath)}} />
-      </Sider>
-    )
-}
 export default SideLayout;
